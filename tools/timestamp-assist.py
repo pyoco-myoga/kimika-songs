205328348c6f5f0495eda3eae131fef66caae4f5
pyoco-myoga <124489297+pyoco-myoga@users.noreply.github.com>
2023-05-06 21:51:35 +0900

    update timestamp tool

--- tools/timestamp-assist.py
+++ tools/timestamp-assist.py
@@ -1,37 +1,73 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import Any, List, Optional, Tuple
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter, base
 import pyperclip
 
 
-def write_command(data: List[datetime]):
-    with open("./timestamp-data.txt", "w", encoding="utf8") as f:
-        f.write(str(data))
+def write_command(data: List[Any], video_id: str, base_time: datetime):
+    with open("./timestamp-data.html", "w", encoding="utf8") as f:
+        html_str = "<table><tbody>"
+        for (t, endt, song_artist) in data:
+            t_delta = t - base_time
+            endt_delta = endt - base_time
+            html_str += f"""<tr>
+                <td>{song_artist}</td>
+                <td><a href={youtube_url(video_id, int(t_delta.total_seconds()))}>start</a></td>
+                <td><a href={youtube_url(video_id, int(endt_delta.total_seconds()))}>start</a></td>
+            </tr>"""
+        html_str += "</tbody></table>"
+        f.write(html_str)
+
+def youtube_url(video_id: str, second: int) -> str:
+    return f"https://youtube.com/watch?v={video_id}&t={second}"
+
+
+def datetime_to_time(t: datetime, base: datetime) -> str:
+    delta = t - base
+    hours = delta.seconds // 3600
+    minutes = (delta.seconds // 60) % 60
+    seconds = delta.seconds % 60
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 if __name__ == "__main__":
     video_id: Optional[str] = None
     base_time: Optional[datetime] = None
-    data: List[datetime] = []
-    command_completer = WordCompleter(["ls", "append", "pop", "timestamp", "url", "base"])
+    data: List[Tuple[datetime, datetime, str]] = []
+    command_completer = WordCompleter([
+        "ls", "add", "pop", "timestamp", "url", "base", "video", "write"
+        ])
     while True:
         try:
             cmd = prompt("> ", completer=command_completer).split()
         except EOFError:
-            break
+            if input("exit? y/n>") == "y":
+                break
+            else:
+                continue
+
         if len(cmd) == 0:
             continue
 
-        if cmd[0] == "exit":
-            break
-
         if cmd[0] == "ls":
-            for t in data:
-                print(t)
-        elif cmd[0] == "append":
+            if base_time is None:
+                print("base_time is not set, please set")
+                continue
+            timestamp_str = ""
+            for (t, endt, song_artist) in data:
+                timestamp_str += \
+                        f"{datetime_to_time(t, base_time)} " \
+                        f"{datetime_to_time(endt, base_time)} " \
+                        f"{song_artist}\n"
+            print(timestamp_str)
+            pyperclip.copy(timestamp_str)
+
+        elif cmd[0] == "add":
             dt = datetime.now()
-            data.append(dt)
+            song_artist = prompt("endt and (song, artist)>")
+            data.append((dt, datetime.now(), song_artist))
+
         elif cmd[0] == "pop":
             print(data.pop())
 
@@ -40,13 +76,8 @@ if __name__ == "__main__":
                 print("base_time is not set, please set")
                 continue
             timestamp_str = ""
-            for t in data:
-                delta = t - base_time
-                hours = delta.seconds // 3600
-                minutes = (delta.seconds // 60) % 60
-                seconds = delta.seconds % 60
-                timestamp = f"{hours:02}:{minutes:02}:{seconds:02}"
-                timestamp_str += timestamp + "\n"
+            for (t, _endt, song_artist) in data:
+                timestamp_str += f"{datetime_to_time(t, base_time)} {song_artist}\n"
             print(timestamp_str)
             pyperclip.copy(timestamp_str)
 
@@ -57,9 +88,13 @@ if __name__ == "__main__":
             if video_id is None:
                 print("video_id is not set, please set")
                 continue
-            for t in data:
+            for (t, endt, song_artist) in data:
                 delta = t - base_time
-                print(f"https://youtube.com/watch?v={video_id}&t={int(delta.total_seconds())}")
+                print(song_artist)
+                print(f"t: {youtube_url(video_id, int(delta.total_seconds()))}")
+                delta = endt - base_time
+                print(f"t: {youtube_url(video_id, int(delta.total_seconds()))}")
+                print()
 
         elif cmd[0] == "base":
             base = prompt(
@@ -73,7 +108,15 @@ if __name__ == "__main__":
 
         elif cmd[0] == "video":
             video_id = prompt("video id> ")
+
+        elif cmd[0] == "write":
+            if base_time is None:
+                print("base_time is not set, please set")
+                continue
+            if video_id is None:
+                print("video_id is not set, please set")
+                continue
+            write_command(data, video_id, base_time)
+
         else:
             print(f"not supported command {cmd[0]}")
-
-    write_command(data)
